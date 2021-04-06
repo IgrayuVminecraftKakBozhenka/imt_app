106861de0de8db37f559c4bb385f5e29003c40b9
danil <danko200028@mail.ru>
2021-04-06 16:31:33 +0600

    first layout done

--- app/src/main/java/com/igrayuvminecraftkakbozhenka/superapp/MainActivity.java
+++ app/src/main/java/com/igrayuvminecraftkakbozhenka/superapp/MainActivity.java
@@ -3,12 +3,26 @@ package com.igrayuvminecraftkakbozhenka.superapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button okButton = findViewById(R.id.main_activity_ok_button);
+        okButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case (R.id.main_activity_ok_button):
+                Toast.makeText(getApplicationContext(), "ok", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
