9346e1025e4597c0a597bff9d4c83876ba8db402
danil <danko200028@mail.ru>
2021-04-08 19:26:32 +0600

    done

--- app/src/main/java/com/igrayuvminecraftkakbozhenka/superapp/MainActivity.java
+++ app/src/main/java/com/igrayuvminecraftkakbozhenka/superapp/MainActivity.java
@@ -2,20 +2,22 @@ package com.igrayuvminecraftkakbozhenka.superapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public final class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private EditText highInput;
-    private EditText weightInput;
     public final static String INTENT_KEY_HIGH = "intent_key_high";
     public final static String INTENT_KEY_WEIGHT = "intent_key_weight";
 
+    private EditText highInput;
+    private EditText weightInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +29,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         final Button okButton = findViewById(R.id.main_activity_ok_button);
         okButton.setOnClickListener(this);
+        highInput.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    v.postDelayed(() -> showKeyboard(v), 200);
+                }
+            }
+        });
         highInput.requestFocus();
 
     }
@@ -52,5 +62,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             intent.putExtra(INTENT_KEY_WEIGHT, userWeight);
             startActivity(intent);
         }
+    }
+    private void showKeyboard(View view) {
+        final InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputManager.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT);
     }
 }
--- app/src/main/java/com/igrayuvminecraftkakbozhenka/superapp/ResultActivity.java
+++ app/src/main/java/com/igrayuvminecraftkakbozhenka/superapp/ResultActivity.java
@@ -1,5 +1,6 @@
 package com.igrayuvminecraftkakbozhenka.superapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,10 +9,10 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ResultActivity extends AppCompatActivity implements View.OnClickListener {
+public final class ResultActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView recomendation;
-    private TextView imt;
+    private TextView tvImt;
     private Button backButton;
 
     @Override
@@ -19,50 +20,60 @@ public class ResultActivity extends AppCompatActivity implements View.OnClickLis
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
-        double high = Double.parseDouble(getIntent().getStringExtra(MainActivity.INTENT_KEY_HIGH));
-        double weigh = Double.parseDouble(getIntent().getStringExtra(MainActivity.INTENT_KEY_WEIGHT));
 
+        final double high = getInitialValue(MainActivity.INTENT_KEY_HIGH);
+        final double weigh = getInitialValue(MainActivity.INTENT_KEY_WEIGHT);
+        final double imt = getImt(high, weigh);
+        final String imtString = Double.toString(imt);
         recomendation = findViewById(R.id.result_activity_recomendation);
-        imt = findViewById(R.id.result_activity_imt);
-        imt.setText(Double.toString(getImt(high, weigh)));
-        recomendation.setText(getEvaluation(getImt(high, weigh)));
+        tvImt = findViewById(R.id.result_activity_imt);
+        tvImt.setText(imtString);
+        recomendation.setText(getEvaluation(imt));
         backButton = findViewById(R.id.result_activity_back_button);
         backButton.setOnClickListener(this);
-
-
     }
 
-    private double getImt(double high, double weigh) {
-        high = high / 100;
-        double result = weigh / (high * high);
-        return result;
+    private double getInitialValue(final String key) {
+        final Intent intent = getIntent();
+        if (intent == null) {
+            return 0.0;
+        }
+        final String value = intent.getStringExtra(key);
+        if (value == null) {
+            return 0.0;
+        }
+        double doubleValue = 0.0;
+        try {
+            doubleValue = Double.parseDouble(value);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+        return doubleValue;
+    }
+
+    private double getImt(final double high, final double weigh) {
+        final double highMeter = high / 100;
+        return weigh / (highMeter * highMeter);
     }
 
     private String getEvaluation(double imt) {
-        String recomend;
+        String recomend = "";
         if (imt < 16) {
             recomend = "Сильный дефицит массы тела, срочно обратитесь к врачу";
-            return recomend;
-        } else if (imt > 16.0 && imt < 18.5) {
+        } else if (imt >= 16.0 && imt <= 18.5) {
             recomend = "Сильный дефицит массы тела, рекомендуется диета";
-            return recomend;
-        } else if (imt > 18.5 && imt < 24.99) {
+        } else if (imt > 18.5 && imt <= 24.99) {
             recomend = "Нормальная масса тела, рекомендуется регулярные занятия спортом";
-            return recomend;
-        } else if (imt > 25 && imt < 30) {
+        } else if (imt >= 25 && imt <= 30) {
             recomend = "Предожирение, рекомендуется диета и занятия спортом";
-            return recomend;
-        } else if (imt > 30 && imt < 35) {
+        } else if (imt > 30 && imt <= 35) {
             recomend = "Ожирение первой степени, обратитесь к врачу, рекомендуется диета и занятия спортом";
-            return recomend;
-        } else if (imt > 35 && imt < 40) {
+        } else if (imt > 35 && imt <= 40) {
             recomend = "Ожирение второй степени, обратитесь к врачу, рекомендуется диета и занятия спортом";
-            return recomend;
         } else if (imt > 40) {
             recomend = "Ожирение третьей степени, вы вообще передвигаетесь?";
-            return recomend;
         }
-        return "";
+        return recomend;
     }
 
     @Override
