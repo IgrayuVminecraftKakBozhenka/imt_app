ca90cdd4d776030187cbe832180cbe65312e160b
danil <danko200028@mail.ru>
2021-04-08 12:44:46 +0600

    layout done

--- app/src/main/java/com/igrayuvminecraftkakbozhenka/superapp/MainActivity.java
+++ app/src/main/java/com/igrayuvminecraftkakbozhenka/superapp/MainActivity.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    final EditText highInput = findViewById(R.id.main_activity_enter_ur_high);
+    final EditText weightInput = findViewById(R.id.main_activity_enter_ur_weight);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,14 +19,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         final Button okButton = findViewById(R.id.main_activity_ok_button);
         okButton.setOnClickListener(this);
+        final EditText highInput = findViewById(R.id.main_activity_enter_ur_high);
+        final EditText weightInput = findViewById(R.id.main_activity_enter_ur_weight);
+        highInput.requestFocus();
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case (R.id.main_activity_ok_button):
-                Toast.makeText(getApplicationContext(), "ok", Toast.LENGTH_SHORT).show();
+                okClick();
+                break;
         }
 
+    }
+
+    private void okClick() {
+        final String userHigh = highInput.getText().toString();
     }
 }
