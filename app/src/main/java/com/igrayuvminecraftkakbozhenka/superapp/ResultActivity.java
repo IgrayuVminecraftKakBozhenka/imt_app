bcfa0d78d32d6727a2cb2c1c551c8badf72877a3
danil <danko200028@mail.ru>
2021-04-08 13:57:44 +0600

    imt result done

--- app/src/main/java/com/igrayuvminecraftkakbozhenka/superapp/ResultActivity.java
+++ app/src/main/java/com/igrayuvminecraftkakbozhenka/superapp/ResultActivity.java
@@ -1,14 +1,36 @@
 package com.igrayuvminecraftkakbozhenka.superapp;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ResultActivity extends AppCompatActivity {
 
+    private TextView recomendation;
+    private TextView imt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        double high = Double.parseDouble(getIntent().getStringExtra(MainActivity.INTENT_KEY_HIGH));
+        double weigh = Double.parseDouble(getIntent().getStringExtra(MainActivity.INTENT_KEY_WEIGHT));
+
+        recomendation = findViewById(R.id.result_activity_recomendation);
+        imt = findViewById(R.id.result_activity_imt);
+
+
+
+        imt.setText(Double.toString(getImt(high, weigh)));
+
+    }
+
+    private double getImt(double high, double weigh) {
+        high = high / 100;
+        double result = weigh / (high * high);
+        return result;
     }
 }
