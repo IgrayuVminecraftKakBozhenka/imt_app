a777b5e2cf253149d103c7aa9d0aff08f9da3da3
danil <danko200028@mail.ru>
2021-04-08 14:33:55 +0600

    evaluation done

--- app/src/main/java/com/igrayuvminecraftkakbozhenka/superapp/ResultActivity.java
+++ app/src/main/java/com/igrayuvminecraftkakbozhenka/superapp/ResultActivity.java
@@ -21,10 +21,9 @@ public class ResultActivity extends AppCompatActivity {
 
         recomendation = findViewById(R.id.result_activity_recomendation);
         imt = findViewById(R.id.result_activity_imt);
-
-
-
         imt.setText(Double.toString(getImt(high, weigh)));
+        recomendation.setText(getEvaluation(getImt(high, weigh)));
+
 
     }
 
@@ -33,4 +32,32 @@ public class ResultActivity extends AppCompatActivity {
         double result = weigh / (high * high);
         return result;
     }
+
+    private String getEvaluation(double imt) {
+        String recomend;
+        if (imt < 16) {
+            recomend = "Сильный дефицит массы тела";
+            return recomend;
+        } else if (imt > 16.0 && imt < 18.5) {
+            recomend = "Сильный дефицит массы тела";
+            return recomend;
+        } else if (imt > 18.5 && imt < 24.99) {
+            recomend = "Нормальная масса тела";
+            return recomend;
+        } else if (imt > 25 && imt < 30) {
+            recomend = "Предожирение";
+            return recomend;
+        } else if (imt > 30 && imt < 35) {
+            recomend = "Ожирение первой степени";
+            return recomend;
+        } else if (imt > 35 && imt < 40) {
+            recomend = "Ожирение второй степени";
+            return recomend;
+        } else if (imt > 40) {
+            recomend = "Ожирение третьей степени";
+            return recomend;
+        }
+        return "";
+    }
+
 }
